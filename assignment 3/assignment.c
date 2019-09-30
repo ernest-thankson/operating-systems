8209ab7ba40b951d59db58996b6d5f3c5683ad70
root <root@DESKTOP-QQAA99L.localdomain>
2019-09-30 11:37:15 +0000

    first commit

---
+++ assignment 3/assignment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159
+
+double calcPolygonArea(int pType, double length){
+	double numerator = pow(length, 2) * pType;
+	double denominator = 4 * sin(PI/pType)/cos(PI/pType);
+	double area = numerator/denominator;
+	return area;
+}
